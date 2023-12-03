1ce24b293ba058748c838dbfb3034b1f791f7c88
Fred Marecesche <fred@froddd.co.uk>
2023-12-03 22:33:25 +0000

    Day 03 part 2

--- 03/index.js
+++ 03/index.js
@@ -32,6 +32,38 @@ const part1 = input => {
 }
 
 const part2 = input => {
+    const gears = input.reduce((gears, line, y) => {
+        const gearRegex = new RegExp('\\*', 'g');
+        const newGears = [];
+        while (gearRegex.exec(line) !== null) {
+            newGears.push({ position: [gearRegex.lastIndex - 1, y], adjacentParts: []});
+        }
+        return [...gears, ...newGears];
+    }, [])
+
+    input.forEach((line, index) => {
+        let lineTotal = 0;
+        const partNumberRegex = new RegExp('[0-9]+', 'g');
+        let match;
+        while ((match = partNumberRegex.exec(line)) !== null) {
+            const partNumber = match[0];
+            const start = match.index;
+            const end = match.index + partNumber.length - 1;
+            const adjacentGear = gears.find(({ position }) => (
+                position[0] >= start - 1
+                && position[0] <= end + 1
+                && position[1] >= index - 1
+                && position[1] <= index + 1)
+            )
+            if (adjacentGear) {
+                adjacentGear.adjacentParts.push(partNumber)
+            }
+        }
+    })
+
+    return gears.filter(gear => gear.adjacentParts.length === 2).reduce((total, gear) => {
+        return total + gear.adjacentParts[0] * gear.adjacentParts[1];
+    }, 0);
 }
 
 module.exports = {
