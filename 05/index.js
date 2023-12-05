b13a065517cf2997f6e9fa34742c13d3ac59ef61
Fred Marecesche <fred@froddd.co.uk>
2023-12-05 11:52:41 +0000

    Day 05 part 2 -- that's slow! :D

--- 05/index.js
+++ 05/index.js
@@ -1,11 +1,5 @@
-const part1 = (input) => {
-  const startSeeds = input
-    .shift()
-    .replace("seeds: ", "")
-    .split(" ")
-    .map(Number);
-
-  const maps = input.reduce((maps, line) => {
+const getMaps = (input) =>
+  input.reduce((maps, line) => {
     if (line.match(/map/)) {
       maps.push([]);
       return maps;
@@ -20,24 +14,59 @@ const part1 = (input) => {
     return maps;
   }, []);
 
-  const endSeeds = startSeeds.map((seed) => {
-    let value = seed;
+const mapSeed = (seed, maps) => {
+  let value = seed;
 
-    maps.forEach((map) => {
-      const translator = map.find((x) => x[1] <= value && x[1] + x[2] >= value);
+  maps.forEach((map) => {
+    const translator = map.find((x) => x[1] <= value && x[1] + x[2] >= value);
 
-      if (translator) {
-        value += translator[0] - translator[1];
-      }
-    });
-
-    return value;
+    if (translator) {
+      value += translator[0] - translator[1];
+    }
   });
+
+  return value;
+};
+
+const mapSeeds = (seeds, maps) => seeds.map((seed) => mapSeeds(seed, maps));
+
+const part1 = (input) => {
+  const startSeeds = input
+    .shift()
+    .replace("seeds: ", "")
+    .split(" ")
+    .map(Number);
+
+  const maps = getMaps(input);
+  const endSeeds = mapSeeds(startSeeds, maps);
 
   return Math.min(...endSeeds);
 };
 
-const part2 = (input) => {};
+const part2 = (input) => {
+  const seedRanges = input
+    .shift()
+    .replace("seeds: ", "")
+    .split(" ")
+    .map(Number);
+
+  let lowestEndSeed = Infinity;
+  let processedSeeds = 0;
+
+  const maps = getMaps(input);
+
+  for (let i = 0; i < seedRanges.length; i += 2) {
+    for (let j = 0; j < seedRanges[i + 1]; j++) {
+      const value = mapSeed(seedRanges[i] + j, maps);
+
+      if (value < lowestEndSeed) lowestEndSeed = value;
+      processedSeeds++;
+    }
+  }
+
+  console.log(`Total seeds processed: ${processedSeeds}`);
+  return lowestEndSeed;
+};
 
 module.exports = {
   part1,
